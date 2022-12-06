b165ae930a9245fcb942457809e61cb3e8b2a189
Yusuke <hassamu1838@gmail.com>
2022-12-06 16:12:23 +0900

    12/6

--- PortApp/form.py
+++ PortApp/form.py
@@ -1,9 +1,10 @@
 from django import forms
 
 class numForm(forms.Form):
-  chicken = forms.DecimalField(label='鶏むね肉(g)', initial=0, required=True)
-  onion = forms.DecimalField(label='玉ねぎ(g)', initial=0, required=True)
-  carrot = forms.DecimalField(label='にんじん(g)', initial=0, required=True)
-  tomato = forms.DecimalField(label='トマト缶(缶)', initial=0, required=True)
-  meals = forms.DecimalField(label='何食分か', initial=1, required=True)
-  rice = forms.DecimalField(label='白米(合)', initial=0, required=True)
+  meals = forms.DecimalField(label='何食分', initial=1, min_value=1, help_text='何食分作るのか入力してください', required=True)
+  chicken = forms.DecimalField(label='鶏むね肉(g)', initial=0, min_value=0, step_size=10, required=True)
+  onion = forms.DecimalField(label='玉ねぎ(g)', initial=0, min_value=0, step_size=10, required=True)
+  carrot = forms.DecimalField(label='にんじん(g)', initial=0, min_value=0, step_size=10, required=True)
+  tomato = forms.DecimalField(label='トマト缶(缶)', initial=0, min_value=0, step_size=0.1, required=True)
+  rice = forms.DecimalField(label='白米(合)', initial=0, min_value=0, step_size=0.25, required=True)
+  egg = forms.DecimalField(label='卵Mサイズ(個)', initial=0, min_value=0, step_size=1, required=True)
--- PortApp/views.py
+++ PortApp/views.py
@@ -12,22 +12,22 @@ def CalculationPageView(request):
 
         if form.is_valid():# バリデーションを行う
 
+            meals=int(form.cleaned_data['meals'])
             chicken=float(form.cleaned_data['chicken'])
             onion=float(form.cleaned_data['onion'])
             carrot=float(form.cleaned_data['carrot'])
             tomato=float(form.cleaned_data['tomato'])
-            meals=int(form.cleaned_data['meals'])
             rice=float(form.cleaned_data['rice'])
-            if meals==0:
-                ans='0以外の数字で、何食分か入力してください'
-            elif meals==1:
-                calorie=round(chicken/100*108 + rice*538 + onion/100*33 + carrot/100*39 + tomato*84,2)
-                protein=round(chicken/100*23 + rice*8 + onion/100*1 + + carrot/100*0.7 + tomato*4.8,2)
+            egg=float(form.cleaned_data['egg'])
+           
+            if meals==1:
+                calorie=round(chicken/100*108 + rice*538 + onion/100*33 + carrot/100*39 + tomato*84 + egg*87,2)
+                protein=round(chicken/100*23 + rice*8 + onion/100*1 + + carrot/100*0.7 + tomato*4.8 + egg*7.4,2)
                 ans=f'[{str(meals)}食分] カロリー: {str(calorie)}kcal \n タンパク質: {str(protein)}g'
             else:
-                calorie=round(chicken/100*108 + rice*538 + onion/100*33 + carrot/100*39 + tomato*84,2)
+                calorie=round(chicken/100*108 + rice*538 + onion/100*33 + carrot/100*39 + tomato*84 + egg*87,2)
                 calorie_meal=round(calorie/meals,2)
-                protein=round(chicken/100*23 + rice*8 + onion/100*1 + + carrot/100*0.7 + tomato*4.8,2)
+                protein=round(chicken/100*23 + rice*8 + onion/100*1 + + carrot/100*0.7 + tomato*4.8 + egg*7.4,2)
                 protein_meal=round(protein/meals,2)
                 ans_meals=f'[{str(meals)}食分] カロリー: {str(calorie)}kcal \n　　　タンパク質: {str(protein)}g\n' 
                 ans_meal=f'[1食分] カロリー: {str(calorie_meal)}kcal \n 　　　タンパク質: {str(protein_meal)}g'
