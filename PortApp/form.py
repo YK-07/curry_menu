16d59f81379e842a1ee5f4c1a15da46005005903
Yusuke <hassamu1838@gmail.com>
2022-12-09 15:50:48 +0900

    12/9

--- PortApp/form.py
+++ PortApp/form.py
@@ -8,3 +8,9 @@ class numForm(forms.Form):
   tomato = forms.DecimalField(label='トマト缶(缶)', initial=0, min_value=0, step_size=0.1, required=True)
   rice = forms.DecimalField(label='白米(合)', initial=0, min_value=0, step_size=0.25, required=True)
   egg = forms.DecimalField(label='卵Mサイズ(個)', initial=0, min_value=0, step_size=1, required=True)
+  sex = forms.DecimalField(label='性別', initial=0, min_value=0, step_size=1, help_text='男性は1を、女性は2を入力してください', required=True)
+  height = forms.DecimalField(label='身長(cm)', initial=0, min_value=0, step_size=1, required=True)
+  weight = forms.DecimalField(label='体重(kg)', initial=0, min_value=0, step_size=1, required=True)
+  age = forms.DecimalField(label='年齢', initial=20, min_value=0,required=True)
+  intensity = forms.DecimalField(label='活動レベル', initial=1.2, min_value=0, step_size=0.15, help_text='ほぼ運動しない(1.2),軽い運動 (1.35),中程度の運動(1.5),激しい運動(1.8)',required=True)
+  
--- PortApp/views.py
+++ PortApp/views.py
@@ -19,21 +19,39 @@ def CalculationPageView(request):
             tomato=float(form.cleaned_data['tomato'])
             rice=float(form.cleaned_data['rice'])
             egg=float(form.cleaned_data['egg'])
-            
+            sex=float(form.cleaned_data['sex'])
+            height=float(form.cleaned_data['height'])
+            weight=float(form.cleaned_data['weight'])
+            age=float(form.cleaned_data['age'])
+            intensity=float(form.cleaned_data['intensity'])
+
+            ans_burn=''
+            if sex!=0 and height!=0 and weight!=0:
+                if sex==1:#男性
+                    basal_metabolism=round(13.397*weight + 4.799*height - 5.677*age + 88.362,2)
+                    calorie_burn=round(basal_metabolism*intensity,2)
+                    protein_cons=round(weight*1.5,2)
+                elif sex==2:#女性
+                    basal_metabolism=round(9.247*weight + 3.098*height - 4.33*age + 447.593,2)
+                    calorie_burn=round(basal_metabolism*intensity,2)
+                    protein_cons=round(weight*1.5,2)
+                
+                ans_burn=f'基礎代謝：約{str(basal_metabolism)}kcal　一日の消費カロリー：約{str(calorie_burn)}kcal　タンパク質量摂取目安：約{protein_cons}g'
+
             calorie=round(chicken/100*108 + rice*538 + onion/100*33 + carrot/100*39 + tomato*84 + egg*87,2)
             protein=round(chicken/100*23 + rice*8 + onion/100*1 + + carrot/100*0.7 + tomato*4.8 + egg*7.4,2)
             ans_meals=f'[{str(meals)}食分] カロリー: {str(calorie)}kcal \n 　　　タンパク質: {str(protein)}g\n'
 
             if meals==1:
-                ans=ans_meals
+                ans=ans_meals + ans_burn
+
             else:
                 calorie_meal=round(calorie/meals,2)
                 protein_meal=round(protein/meals,2)
-                ans_meal=f'[1食分] カロリー: {str(calorie_meal)}kcal \n 　　　タンパク質: {str(protein_meal)}g'
-                ans=ans_meals + ans_meal
+                ans_meal=f'[1食分] カロリー: {str(calorie_meal)}kcal \n 　　　タンパク質: {str(protein_meal)}g\n'
+                ans=ans_meals + ans_meal + ans_burn
     else:
         form=numForm()
         ans=""
     return render(request, "calculation.html",{'form':form, 'ans' : ans})
-
 
